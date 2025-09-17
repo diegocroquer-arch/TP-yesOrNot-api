4d50dc45c197f845cc33d9db4b6e6378b16e4505
Diego Croquer <diego.croquer@gm2dev.com>
2025-09-17 11:28:58 -0300

    Agregar estructura HTML, estilos CSS y funcionalidad JavaScript para la API Fetch

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 const url = "https://yesno.wtf/api";
+
+const giftImg = document.querySelector("img");
+const text = document.querySelector("p");
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    giftImg.src = data.image;
+    text.textContent = data.answer;
+  });
+
+const button = document.getElementById("random");
+
+button.addEventListener("click", () => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      giftImg.src = data.image;
+      text.textContent = data.answer;
+    });
+});
+
+const button_yes = document.getElementById("yes");
+const button_no = document.getElementById("no");
+const button_maybe = document.getElementById("maybe");
+
+button_yes.addEventListener("click", () => {
+  fetch(url + "?force=yes")
+    .then((response) => response.json())
+    .then((data) => {
+      giftImg.src = data.image;
+      text.textContent = data.answer;
+    });
+});
+button_no.addEventListener("click", () => {
+  fetch(url + "?force=no")
+    .then((response) => response.json())
+    .then((data) => {
+      giftImg.src = data.image;
+      text.textContent = data.answer;
+    });
+});
+button_maybe.addEventListener("click", () => {
+  fetch(url + "?force=no")
+    .then((response) => response.json())
+    .then((data) => {
+      giftImg.src = data.image;
+      text.textContent = data.answer;
+    });
+});
