b2e6741a12d74888874a43df9c595db3ee2c2ab5
Diego Croquer <diego.croquer@gm2dev.com>
2025-09-17 11:53:11 -0300

    Refactor la función de obtención de datos para simplificar el manejo de respuestas

--- script.js
+++ script.js
@@ -1,51 +1,29 @@
-const url = "https://yesno.wtf/api";
+const BASE_URL = "https://yesno.wtf/api";
 
 const giftImg = document.querySelector("img");
 const text = document.querySelector("p");
 
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    giftImg.src = data.image;
-    text.textContent = data.answer;
-  });
-
-const button = document.getElementById("random");
-
-button.addEventListener("click", () => {
+function getData(force) {
+  const url = force ? `${BASE_URL}?force=${force}` : BASE_URL;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       giftImg.src = data.image;
       text.textContent = data.answer;
     });
-});
+}
+
+const button = document.getElementById("random");
+
+getData();
+button.addEventListener("click", () => getData("random"));
 
 const button_yes = document.getElementById("yes");
 const button_no = document.getElementById("no");
 const button_maybe = document.getElementById("maybe");
 
-button_yes.addEventListener("click", () => {
-  fetch(url + "?force=yes")
-    .then((response) => response.json())
-    .then((data) => {
-      giftImg.src = data.image;
-      text.textContent = data.answer;
-    });
-});
-button_no.addEventListener("click", () => {
-  fetch(url + "?force=no")
-    .then((response) => response.json())
-    .then((data) => {
-      giftImg.src = data.image;
-      text.textContent = data.answer;
-    });
-});
-button_maybe.addEventListener("click", () => {
-  fetch(url + "?force=no")
-    .then((response) => response.json())
-    .then((data) => {
-      giftImg.src = data.image;
-      text.textContent = data.answer;
-    });
-});
+button_yes.addEventListener("click", () => getData("yes"));
+
+button_no.addEventListener("click", () => getData("no"));
+
+button_maybe.addEventListener("click", () => getData("maybe"));
